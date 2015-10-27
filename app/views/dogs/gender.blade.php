ea3e3dff3fab5843658d20eb2338af2f1ab1bf15
Remington <remingtonwilliams@gmail.com>
2015-10-26 20:09:13 -0500

    change file extensions

--- app/views/dogs/gender.blade.php
+++ app/views/dogs/gender.blade.php
@@ -17,7 +17,7 @@
     <div class="row">
         <a href="{{{action('DogsController@retired', $key->gender)}}}">
             <h3 class="centeredText">Retired {{{ $key->gender}}}s</h3>
-            <img class="img-responsive col-md-6 col-md-offset-3 thumbnail" src="/img/dogs/retired/{{{ $key->gender }}}s.jpg" alt="">
+            <img class="img-responsive col-md-6 col-md-offset-3 thumbnail" src="/img/dogs/retired/retired{{{ $key->gender }}}s.jpg" alt="">
         </a>
     </div>
 @stop
