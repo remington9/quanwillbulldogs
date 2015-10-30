157b45d44f6414b431885359ce91a7df0f248e3d
Remington <remingtonwilliams@gmail.com>
2015-10-30 14:33:27 -0500

    fixed paypal

--- app/views/contact.blade.php
+++ app/views/contact.blade.php
@@ -13,7 +13,13 @@
     <h1 class="canevalee centeredText">Make Deposits. Pay Online,</h1>
     <h1 class="canevalee centeredText"> It’s Easy and Secure With PayPal</h1>
     <div class="row">
-        <a href="https://www.paypal.com/us/cgi-bin/webscr?cmd=_flow&SESSION=ArduiRomFLLrcAu3100DEQHmYJ0Vru8skZLF5qJz5l-Hv-y4GxTSSeQw0JG&dispatch=50a222a57771920b6a3d7b606239e4d529b525e0b7e69bf0224adecfb0124e9b61f737ba21b081984719ecfa9a8ffe80733a1a700ced90ae"><img src="/img/paypal.jpg" class="col-md-4 col-md-offset-4 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1" alt="paypal"></a>
+        <div class="col-md-4 col-md-offset-4 col-xs-10 col-xs-offset-1">
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+                <input type="hidden" name="cmd" value="_s-xclick">
+                <input type="hidden" name="hosted_button_id" value="3543446">
+                <input type="image" src="/img/paypal.jpg" class="col-md-12 col-xs-10 center" name="submit" alt="PayPal - The safer, easier way to pay online!">
+            </form>
+        </div>
     </div>
     <hr>
     <div class="jumbotron">
@@ -27,7 +33,7 @@
         <img class="thumbnail col-md-4 col-sm-12 col-xs-12" src="/img/contactPage/0.jpg" alt="">
         <h4 class="centeredText col-md-4 col-sm-12 col-xs-12">
             Bullnanza Bulldogges<br>
-            Balverde, Tx 78163
+            Bulverde, Tx 78163
         </h4>
         <img class="thumbnail col-md-3 col-md-offset-1 col-sm-12 col-xs-12" src="/img/contactPage/bath.png" alt="">
     </div>
