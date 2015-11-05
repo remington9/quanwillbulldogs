f85bc56e75b6ebb2f9cf103dccb465fb6fcff871
Remington <remingtonwilliams@gmail.com>
2015-11-05 15:28:18 -0600

    fixed the image rows

--- app/views/dogs/fun.blade.php
+++ app/views/dogs/fun.blade.php
@@ -4,21 +4,21 @@
 @stop
 @section('content')
 <div class="row">
-        <?php $i=1; ?>
+        <?php $i=0; ?>
         @foreach($dogs as $key)
             @if($i < 4)
                    <div class="col-md-3 col-xs-12 ">
                        <img class="thumbnail img-responsive" src="/img/dogs/{{{ $key->img_url }}}" alt="">
-                   @if(Auth::id() == $key->user_id)
-                       <a href="{{{action('DogsController@edit', $key->id)}}}" class="btn btn-warning btn-block">Edit</a>
-                   @endif
+                       @if(Auth::id() == $key->user_id)
+                           <a href="{{{action('DogsController@edit', $key->id)}}}" class="btn btn-warning btn-block">Edit</a>
+                       @endif
                    </div>
                    @if($key->img_url2 != '')
                        <div class=" col-md-3 col-xs-12">
                            <img class="img-responsive thumbnail" src="/img/dogs/{{{ $key->img_url2 }}}" alt="">
-                       @if(Auth::id() == $key->user_id)
-                           <a href="{{{action('DogsController@edit', $key->id)}}}" class="btn btn-warning btn-block">Edit</a>
-                       @endif
+                           @if(Auth::id() == $key->user_id)
+                               <a href="{{{action('DogsController@edit', $key->id)}}}" class="btn btn-warning btn-block">Edit</a>
+                           @endif
                        </div>
                        <?php $i++ ?>
                    @endif
@@ -26,33 +26,35 @@
             @else
                 </div>
                 <div class="row">
+                   <?php $i=0 ?>
                    <div class="col-md-3 col-xs-12 ">
                        <img class="img-responsive thumbnail " src="/img/dogs/{{{ $key->img_url }}}" alt="">
-                   @if(Auth::id() == $key->user_id)
-                       <a href="{{{action('DogsController@edit', $key->id)}}}" class="btn btn-warning btn-block">Edit</a>
-                   @endif
+                       @if(Auth::id() == $key->user_id)
+                           <a href="{{{action('DogsController@edit', $key->id)}}}" class="btn btn-warning btn-block">Edit</a>
+                       @endif
                    </div>
-                   <?php $i=1 ?>
+                   <?php $i++ ?>
                    @if($key->img_url2 != '')
                     @if($i < 4)
                        <div class=" col-md-3 col-xs-12">
                            <img class="img-responsive thumbnail " src="/img/dogs/{{{ $key->img_url2 }}}" alt="">
-                       @if(Auth::id() == $key->user_id)
-                           <a href="{{{action('DogsController@edit', $key->id)}}}" class="btn btn-warning btn-block">Edit</a>
-                       @endif
+                           @if(Auth::id() == $key->user_id)
+                               <a href="{{{action('DogsController@edit', $key->id)}}}" class="btn btn-warning btn-block">Edit</a>
+                           @endif
                        </div>
-                       <?php $i+1 ?>
+                       <?php $i++ ?>
                     @else
-                  </div>
-                    <div class="row">
-                      <div class=" col-md-3 col-xs-12">
-                          <img class="img-responsive thumbnail " src="/img/dogs/{{{ $key->img_url2 }}}" alt="">
-                      @if(Auth::id() == $key->user_id)
-                          <a href="{{{action('DogsController@edit', $key->id)}}}" class="btn btn-warning btn-block">Edit</a>
-                      @endif
                       </div>
-                      <?php $i=1 ?>
-                    </div>
+                      <div class="row">
+                        <?php $i=0 ?>
+                        <div class=" col-md-3 col-xs-12">
+                            <img class="img-responsive thumbnail " src="/img/dogs/{{{ $key->img_url2 }}}" alt="">
+                            @if(Auth::id() == $key->user_id)
+                                <a href="{{{action('DogsController@edit', $key->id)}}}" class="btn btn-warning btn-block">Edit</a>
+                            @endif
+                        </div>
+                        <?php $i++ ?>
+                      </div>
                     @endif
                    @endif
                 </div>
