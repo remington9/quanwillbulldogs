ab2eb47a0ed10ffd683dca5dc2465b13781a2de2
Remington <remingtonwilliams@gmail.com>
2015-11-05 17:58:15 -0600

    fixed the image rows

--- app/views/dogs/fun.blade.php
+++ app/views/dogs/fun.blade.php
@@ -9,10 +9,10 @@
         @endif     
         <div class=" col-md-3 col-xs-12">
            <img class="img-responsive thumbnail " src="/img/dogs/{{{ $pics[$i] }}}" alt="">
+            @if(Auth::id())
+               <a href="{{{action('DogsController@edit', $dogsId[$i] )}}}" class="btn btn-warning btn-block">Edit</a>
+            @endif
         </div>
-        @if(Auth::id())
-           <a href="{{{action('DogsController@edit', $dogsId[$i] )}}}" class="btn btn-warning btn-block">Edit</a>
-        @endif
         <?php $i++ ?>
         @if($i%4 == 0 && $i>0)
             </div>
